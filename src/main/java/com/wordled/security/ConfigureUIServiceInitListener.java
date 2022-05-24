4bf62e4c8176b83456226ef04d499b5edf8e789c
JT <59325232+jfemrite@users.noreply.github.com>
2022-05-24 14:31:21 -0500

    Update ConfigureUIServiceInitListener.java

--- src/main/java/com/wordled/security/ConfigureUIServiceInitListener.java
+++ src/main/java/com/wordled/security/ConfigureUIServiceInitListener.java
@@ -1,2 +1,27 @@
-package com.wordled.security;public class ConfigureUIServiceInitListener {
+package com.wordled.security;
+
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.router.BeforeEnterEvent;
+import com.vaadin.flow.server.ServiceInitEvent;
+import com.vaadin.flow.server.VaadinServiceInitListener;
+import com.wordled.views.LoginView;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ConfigureUIServiceInitListener implements VaadinServiceInitListener {
+
+    @Override
+    public void serviceInit(ServiceInitEvent event) {
+        event.getSource().addUIInitListener(uiEvent -> {
+            final UI ui = uiEvent.getUI();
+            ui.addBeforeEnterListener(this::authenticateNavigation);
+        });
+    }
+
+    private void authenticateNavigation(BeforeEnterEvent event) {
+        if (!LoginView.class.equals(event.getNavigationTarget())
+                && !SecurityUtils.isUserLoggedIn()) {
+            event.rerouteTo(LoginView.class);
+        }
+    }
 }
