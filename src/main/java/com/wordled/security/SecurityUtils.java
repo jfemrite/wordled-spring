50b0292ab2b33eb7683aaf91ec547cde7e4ab2f4
JT <59325232+jfemrite@users.noreply.github.com>
2022-05-24 14:35:27 -0500

    Update SecurityUtils.java

--- src/main/java/com/wordled/security/SecurityUtils.java
+++ src/main/java/com/wordled/security/SecurityUtils.java
@@ -1,2 +1,40 @@
-package com.wordled.security;public class SecurityUtils {
+package com.wordled.security;
+
+import com.vaadin.flow.server.HandlerHelper.RequestType;
+import com.vaadin.flow.shared.ApplicationConstants;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.stream.Stream;
+
+public final class SecurityUtils {
+
+    private SecurityUtils() {
+        // Util methods only
+    }
+
+    /**
+     * Determine if request is interal
+     * @param request
+     * @return
+     */
+    static boolean isFrameworkInternalRequest(HttpServletRequest request) {
+        final String parameterValue = request.getParameter(ApplicationConstants.REQUEST_TYPE_PARAMETER);
+        return parameterValue != null
+                && Stream.of(RequestType.values())
+                .anyMatch(r -> r.getIdentifier().equals(parameterValue));
+    }
+
+    /**
+     * Checks if the user is currently logged in
+     * @return true if user is logged in currently
+     */
+    public static boolean isUserLoggedIn() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return authentication != null
+                && !(authentication instanceof AnonymousAuthenticationToken)
+                && authentication.isAuthenticated();
+    }
 }
